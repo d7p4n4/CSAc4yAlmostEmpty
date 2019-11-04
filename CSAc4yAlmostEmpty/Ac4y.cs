b271736cd3c814b01730a96b9bb41228a454fe08
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-04 12:26:17 +0000

    1.20191104.1

--- CSAc4yAlmostEmpty/Ac4y.cs
+++ CSAc4yAlmostEmpty/Ac4y.cs
@@ -1,8 +1,9 @@
+using CSAc4y.Class;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CSAc4y.Class
+namespace CSAc4yAlmostEmpty.Class
 {
 	public class Ac4y : Ac4yAlgebra
 	{
